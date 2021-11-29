5d93449cf8677e5863d0faa53d0d4bf190fdd59a
ade maulani bilgis <iiigis0422@gmail.com>
2021-11-29 15:01:49 +0700

    menambahkan ubah elemen

---
+++ ubah_element.py
@@ -0,0 +1,10 @@
+nama=['abel','lando','dipa','rayi','rama']
+print()
+print('element :',nama)
+print()
+nama[4]='agoy'
+print('element ke-4 yang sudah di ubah :',nama)
+print()
+nama[4:]=['duta','sky']
+print('mengubah element ke-4 dan seterusnya :',nama)
+print()
